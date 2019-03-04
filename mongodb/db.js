d0b91a95913dc8cb48e1c307b49998cf42d48b65
Uriel Hayon <uri.a77aque@gmail.com>
2019-03-04 19:30:01 -0300

    Try catch connecting mongo

--- mongodb/db.js
+++ mongodb/db.js
@@ -2,14 +2,18 @@ const MongoClient = require('mongodb').MongoClient;
 
 const connect = () => {
   return new Promise((resolve, reject) => {
-    const client = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true });
-    return client.connect((err, client) => {
-      if (err) {
-        return reject(`Could not connect to the server: ${err}`);
-      } else {
-        return resolve(client);
-      }
-    });
+    try {
+      const client = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true });
+      return client.connect((err, client) => {
+        if (err || !client) {
+          return reject(`Could not connect to the server: ${err}`);
+        } else {
+          return resolve(client);
+        }
+      });
+    } catch(ex) {
+      return reject(`Connection Error ${ex}`)
+    }
   });
 }
 
@@ -19,10 +23,10 @@ const readFromCollection = ({ collection, limit }) => {
       .then(client => {
         const db = client.db(process.env.MONGO_DB_NAME);
         db.collection(collection).find().limit(limit || 0).toArray((err, docs) => {
+          client.close();
           if (err) {
             reject(`Error retrieving ${collection}: ${err}`);
           } else {
-            client.close();
             resolve(docs);
           }
         });
